cf6c2d548daac75213d37e6454e2fb3fdd29882f
User <User@DESKTOP-7POAIM9>
2022-02-20 20:28:05 +0800

    加入專案檔案。

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using SingletonDemo;
+
+TableServers serverList1 = TableServers.GetTableServer();
+TableServers serverList2 = TableServers.GetTableServer();
+
+Console.WriteLine("Start Serving...");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"The next server is: { serverList1.GetWhoIsServer()} from server list 1");
+    Console.WriteLine($"The next server is: { serverList2.GetWhoIsServer()} from server list 2");
+}
+
+Console.WriteLine("End Serving...");
---
+++ SingletonDemo/TableServers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    public class TableServers
+    {
+        private static readonly TableServers instance = new();
+        private List<string> servers = new();
+        private int serverNum = 0;
+        public TableServers()
+        {
+            servers.Add("Johnny");
+            servers.Add("Marry");
+            servers.Add("David");
+        }
+
+        public static TableServers GetTableServer()
+        {
+            return instance;
+        }
+
+        public string GetWhoIsServer()
+        {
+            string result = servers[serverNum];
+
+            serverNum++;
+
+            if(serverNum >= servers.Count) serverNum = 0;
+
+            return result;
+        }
+    }
+}
